846864ad5868926b5757782cb3c6b37f52304df0
Henrique <160255786+Batoreia1@users.noreply.github.com>
2025-09-01 16:50:23 -0400

    telas calendario,matricula,estudantes

--- js/router.js
+++ js/router.js
@@ -14,7 +14,16 @@ const routes = {
     '#diario': '/views/diario/diario.html',
 
     '#professores': '/views/professores/professores.html',
-    
+
+  '#calendario': '/views/calendario/calendario.html',
+  '#registro_eventos': '/views/calendario/registro_eventos.html',
+
+  '#estudantes': '/views/estudantes/estudantes.html',
+  '#editar_aluno': '/views/estudantes/editar_aluno.html',
+  '#tratamento_especial': '/views/estudantes/tratamento_especial.html',
+
+  '#matriculas': '/views/matriculas/matriculas.html',
+  '#matricula_aluno': '/views/matriculas/matricula_aluno.html'
   };
   
 
